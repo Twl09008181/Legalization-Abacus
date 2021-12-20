deb258899cdb17b6e4a36b8b0f77368a17a24890
twl <twl0985314726@gmail.com>
2021-12-20 15:23:43 +0800

    change clusters , still have bugs

--- header/abacus.hpp
+++ header/abacus.hpp
@@ -23,23 +23,93 @@ struct node{
 	int x,y;
 	std::string name;
 };
+// abacus datastructure.
+struct cluster{
+    int xc; // start x-position 
+    // abacus dp
+    int ec; // ec <- ec + e(i)
+    int qc; // qc <- qc +e(i)[x'(i) -wc ]
+    int wc; // wc <- wc + w(i)
+    cluster(int x)
+        :xc{x},ec{0},qc{0},wc{0}{}
+    // if overlap with predecessor cluster, then do cluster.
+    // nodes for caculate each position x
+    std::list<node*>nodes;
+    void AddCell(node*n){
+        nodes.push_back(n);
+        ec += n->weight;
+        qc += n->weight * (n->origin_x - wc);
+        wc += n->width;
+    }
+    void AddCluster(cluster*other)
+    {
+        for(auto n:other->nodes)nodes.push_back(n);
+        ec += other->ec;
+        qc += (other->qc - other->ec * wc);
+        wc += other->wc;
+    }
+};
+
 struct subrow{
 	subrow(int startx,int width,int coordinate)
-		: x1{startx},x2{startx + width},y{coordinate}{remainSpace = x2-x1;}
-	void insert(node*n){
-		nodes.push_back(n);
-		remainSpace-=n->width;
-		if(remainSpace < 0){
-			std::cerr<<"subrow insert error\n";
+		: x1{startx},x2{startx + width},y{coordinate}
+	{
+		remainSpace = x2-x1;
+		cost = 0;
+		clusterNum = BackupNum=0;
+	}
+	cluster& last(){
+		if(empty()){
+			std::cerr<<"error in cluster&last(),Clusters is empty\n";
 			exit(1);
 		}
+		return Clusters.at(clusterNum-1);
 	}
-	std::vector<node*>RipUp(int threshold);//RipUp  the width <= threshold
-	std::pair<int,int> placeRow(node*n=nullptr);
-	std::list<node*>nodes;
+	bool empty(){return clusterNum==0;}
+	void AppendCluster(int);
+	void Collapse();//Collapse start from last Cluster in Clusters, and update ClusterIdx.
+	void place(node*);//Place node to last Cluster in Clusters .
+	int  getPos();//return the total Cost in Clusters.
+
+	std::vector<cluster>Clusters;
 	int x1,x2;
 	int remainSpace;
 	int y;
+	int cost;
+	int clusterNum;// point to last Cluster in Clusters.
+
+	void Backup()
+	{
+		if(clusterNum > ClustersBackup.size())//need allocate 
+			ClustersBackup.push_back(Clusters.back());
+		
+		if(clusterNum > ClustersBackup.size()){
+			std::cerr<<"Back up error,clusterNum > clusterBack.size()\n";
+			exit(1);
+		}
+		BackupNum = clusterNum;
+		ClustersBackup.at(BackupNum-1) = Clusters.at(clusterNum-1);
+	}
+
+	//each time after calling subrow::place(node*) , need recover .
+	void RecvoerClusters(){
+		if(clusterNum==BackupNum)//only need recover last cluster.
+			Clusters.at(clusterNum-1) = ClustersBackup.at(clusterNum-1);
+		else if(clusterNum < BackupNum) // clusters call collapse during place(node*)
+		{
+			if(Clusters.size() < BackupNum)
+			{
+				std::cerr<<"Clusters.size() not enough!\n";
+				exit(1);
+			}
+			for(int idx = clusterNum-1;idx < BackupNum;idx++)
+				Clusters.at(idx) = ClustersBackup.at(idx);
+		}
+		clusterNum = BackupNum;
+	}
+private:
+	int BackupNum;// point to last Cluster in Backup
+	std::vector<cluster>ClustersBackup;
 };
 
 using fixed_node = node;
--- src/abacus.cpp
+++ src/abacus.cpp
@@ -32,51 +32,25 @@ void row::block(fixed_node &terminal){
     if(condition > 1) last->remainSpace = last->x2 - last->x1;
 }
 
-// abacus datastructure.
-struct cluster{
-    int xc; // start x-position 
-    // abacus dp
-    int ec; // ec <- ec + e(i)
-    int qc; // qc <- qc +e(i)[x'(i) -wc ]
-    int wc; // wc <- wc + w(i)
-    cluster(int x,cluster*pred)
-        :xc{x},ec{0},qc{0},wc{0},predecessor{pred}{}
-    // if overlap with predecessor cluster, then do cluster.
-    cluster * predecessor;
-    // nodes for caculate each position x
-    std::list<node*>nodes;
-    void AddCell(node*n){
-        nodes.push_back(n);
-        ec += n->weight;
-        qc += n->weight * (n->origin_x - wc);
-        wc += n->width;
-    }
-    void AddCluster(cluster*other)
-    {
-        for(auto n:other->nodes)nodes.push_back(n);
-        ec += other->ec;
-        qc += (other->qc - other->ec * wc);
-        wc += other->wc;
-    }
-};
 
 
-cluster* Collapse(cluster *c,int min_x,int max_x){
-    c->xc = c->qc / c->ec; // get new position
-    // subject to row range
-    if(c->xc < min_x)c -> xc = min_x;
-    if(c->xc > max_x - c->wc)c -> xc = max_x - c->wc;
+void subrow::Collapse(){
+    cluster& lastC = last();
+    lastC.xc = lastC.qc / lastC.ec;
+    if(lastC.xc < x1) lastC.xc = x1;
+    if(lastC.xc > x2 - lastC.wc)lastC.xc = x2 - lastC.wc;
+
+    int c = clusterNum - 1;
     // check if overlap happend.
-    cluster * finalCluster = c;
-    cluster * pred = c->predecessor;
-    if(pred){
-        if(pred->xc + pred->wc > c->xc){
-            pred->AddCluster(c);
-            delete c;
-            finalCluster = Collapse(pred,min_x,max_x); 
-        }
+    for(;c > 0;c--){
+        cluster&cur = Clusters.at(c);
+        cluster&pred = Clusters.at(c-1);
+        if(pred.xc + pred.wc > cur.xc)
+            pred.AddCluster(&cur);
+        else
+            break;
     }
-    return finalCluster;
+    clusterNum = c + 1;
 }
 
 // modified part : boundary fixed.
@@ -88,59 +62,52 @@ int modify_x(int x1,int x2,node*n){
     return n->origin_x;
 }
 
-// place the node n in subrow r , and return the last Cluster in this subrow.
-cluster* place(cluster*lastC,node *n,subrow*r)
-{
-    int m_x = modify_x(r->x1,r->x2,n);// abacus fixed.
-    //look ahead.
-    if(!lastC || lastC->xc + lastC->wc <= m_x){ // if do not need to compact.
-        lastC = new cluster(m_x,lastC);
-        lastC->AddCell(n);
-        //std::cout<<"create cluster, xc:"<<lastC->xc<<" width:"<<lastC->wc<<"\n";
+void subrow::AppendCluster(int x){
+    if(Clusters.size()==clusterNum){//need allocate new memmory.
+        Clusters.push_back(cluster{x});
     }
-    else{// compactation is best.
-        lastC->AddCell(n);
-        lastC = Collapse(lastC,r->x1,r->x2);// need to check whether the new movement will touch the pred. 
-        //std::cout<<"collapse cluster, xc:"<<lastC->xc<<" width:"<<lastC->wc<<"\n";
+    else if(Clusters.size() > clusterNum)//update clusterNum
+        Clusters.at(clusterNum) = cluster{x};
+    else{
+        std::cerr<<"subrow::AppendCluster error, clusterNum > clusters.size()\n";
+        exit(1);
     }
-    return lastC;
+    clusterNum++;
 }
-// placeRow helper function : update the node's position and return the cost  sum (x-x')^2
-int getPos(cluster*lastC,int y){
+
+
+void subrow::place(node*n){
+    int m_x = modify_x(x1,x2,n);// abacus fixed.
+    if(empty() || last().xc + last().wc <= m_x){//if do not need place compactly.
+        AppendCluster(m_x);//new cluster
+        last().AddCell(n);//new cluster add cell
+    }
+    else{
+        last().AddCell(n);
+        Collapse();//recursive Collapse
+    }
+}
+
+int subrow::getPos(){
     int cost = 0;
-    while(lastC){
-        int x = lastC->xc;
-        for(node* n:lastC->nodes){
+    for(int c = 0;c < clusterNum;c++){
+        cluster& clst = Clusters.at(c);
+        int x = clst.xc;
+        for(node*n : clst.nodes){
             n->x = x;
+            n->y = y;
             cost+=std::abs(n->x - n->origin_x);
-            cost+=std::abs(n->origin_y - y);
+            cost+=std::abs(n->y - n->origin_y);
             x += n->width;
         }
-        lastC = lastC->predecessor;
     }
     return cost;//total cost
 }
 
-// can be called by getting the final position and cost or called trially.(with non-nullptr n)
-std::pair<int,int> subrow::placeRow(node*n){
-    cluster* lastC = nullptr;
-    for(auto ptr = nodes.begin();ptr!=nodes.end();++ptr)
-        lastC = place(lastC,*ptr,this);
-    
-    //caculate position and cost.
-    int cost1 = getPos(lastC,y);//get the original cost
-    int cost2 = cost1;
-    if(n){
-        lastC = place(lastC,n,this);// get the new cost
-        cost2 = getPos(lastC,y);
-    }
-    //memory free.
-    while(lastC){
-        cluster*tmp = lastC;
-        lastC = lastC->predecessor;
-        delete tmp;
-    }
-    return {cost1,cost2};
+
+int SubRowPlace(subrow*r,node*n){
+    r->place(n);
+    return r->getPos();
 }
 
 
@@ -157,14 +124,15 @@ int BsSub(std::vector<subrow>&subs,node*n){
     return std::max(0,l);
 }
 
-bool tryPlace(subrow&sub,node*n,int&bestCost,subrow*&bestp)
-{
+bool tryPlace(subrow&sub,node*n,int&bestCost,subrow*&bestp){
     if(sub.remainSpace >= n->width){//still have space
-        auto cost = sub.placeRow(n);
-        int deltaCost = cost.second - cost.first;//new - original : cost of placing one node.
-        if(deltaCost < bestCost){
+        sub.place(n);
+        int afterCost = sub.getPos();
+        int dCost = afterCost - sub.cost;//new - original : cost of placing one node.
+        sub.RecvoerClusters();
+        if(dCost < bestCost){
             bestp = &sub;
-            bestCost = deltaCost;
+            bestCost = dCost;
             return true;
         }
         else 
@@ -199,7 +167,7 @@ int row::getCost()
 {
     int cost = 0;
     for(auto &sub:subrows)
-        cost += sub.placeRow().first;// call placeRow with nullptr.
+        cost += sub.getPos();// get final position
     return cost;
 }
 int row::getRemain()
@@ -208,61 +176,6 @@ int row::getRemain()
     for(auto sub:subrows)space+=sub.remainSpace;
     return space;
 }
-// rip up the node with width < threshod and return.
-std::vector<node*> subrow::RipUp(int threshold){
-    std::vector<node*>candidate;
-    nodes.remove_if([this,&candidate,threshold](node*n){
-        if(n->width < threshold){
-            candidate.push_back(n);
-            remainSpace+=n->width;
-            return true;
-        }
-        return false;
-    });
-    return candidate;
-}
-std::vector<node*> row::RipUp(int threshold){
-    std::vector<node*>candidate;
-    for(auto &sub:subrows)
-    {
-        auto subCand = sub.RipUp(threshold);
-        for(auto c:subCand)
-            candidate.push_back(c);
-    }
-    std::sort(candidate.begin(),candidate.end(),[](node*n1,node*n2){return n1->x < n2->x;});
-    return candidate;
-}
-
-
-std::vector<node*>Refine(std::vector<node*>&nodes,row&r){
-	std::vector<node*>failed;
-	for(auto n:nodes){
-		auto result = r.placeRow(n);
-		if(result.first==nullptr){
-			failed.push_back(n);
-		}
-		else{
-			result.first->insert(n);//place
-            n->y = r.y;
-        }
-	}
-	std::sort(failed.begin(),failed.end(),[](node* n1,node*n2){return n1->width > n2->width;});
-	return failed;
-}
-bool RefinementPlace(std::vector<node*>&notDone,std::vector<row>&rows){
-    std::sort(notDone.begin(),notDone.end(),[](node*n1,node*n2){return n1->width > n2->width;});
-    for(int round = 0;round<10;round++)
-    for(int i = 0;i < rows.size() && !notDone.empty();i++)
-    {
-        auto ripup = rows.at(i).RipUp((*notDone.begin())->width);
-        notDone = Refine(notDone,rows.at(i));
-        auto notDone2 = Refine(ripup,rows.at(i));
-        for(auto n:notDone2)
-            notDone.push_back(n);
-    }
-    return notDone.empty();
-}
-
 
 int binarySearchRow(std::vector<row>&rows,node*n)
 {
@@ -283,8 +196,8 @@ bool tryPlace2(std::vector<row>&rows,int i,node*n,int&bestCost,subrow*&bestPlace
 {
     row& r = rows.at(i);
     auto place = r.placeRow(n);
-    if(place.first && place.second + std::abs(r.y - n->origin_y) < bestCost){
-        bestCost = place.second + std::abs(r.y - n->origin_y);
+    if(place.first && place.second < bestCost){
+        bestCost = place.second;
         bestPlace = place.first;
         bestRow = i;
         return true;
@@ -303,7 +216,7 @@ int abacus(std::vector<node*>nodes,std::vector<row>&rows){
         }
     );
 
-    std::vector<node*>notDone;notDone.reserve(nodes.size()/10);
+    bool succ = true;
     for(auto n : nodes){
         int bestCost = INT_MAX;
         subrow* bestplace = nullptr;
@@ -324,31 +237,28 @@ int abacus(std::vector<node*>nodes,std::vector<row>&rows){
         }
 
         if(bestplace){
-            bestplace->insert(n);
-            n->y = rows.at(bestRow).y;
+            bestplace->place(n);
+            bestplace->Backup();
+            bestplace->cost = bestplace->getPos();
+            bestplace->remainSpace-=n->width;
+            if(bestplace->remainSpace<0){ 
+                std::cerr<<"abacus remainspace<0!\n";
+                exit(1);
+            }
         }
         else{
-            notDone.push_back(n);
+            succ = false;
+            break;
         }
     }
-    bool done = true;
-    if(!notDone.empty()){
-        /*std::cout<<"before refinement\n";
-		for(auto n:nodes){
-			std::cout<<"pos : "<<n->x<<" "<<n->y<<" ";
-			std::cout<<"shape:"<<n->width<<" "<<n->height<<"\n";
-		}
-   */     
-        std::cout<<"do refine\n";
-        done = RefinementPlace(notDone,rows);
-    }
-
-    if(done){ 
-        //updating coordinate and return cost
+    if(succ){
         int cost = 0;
         for(auto &r:rows)
             cost+=r.getCost();
         return cost;
     }
-    return -1;
+    else{
+        std::cout<<"abacus failed\n";
+        return -1;
+    }
 }
